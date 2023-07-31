43d5ab940358ae9228407f25aab8d2b56d0c1445
anjaligurjar <anjaligurjar861@gmail.com>
2023-07-31 17:10:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-fish
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-4bwyel)
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+ 
+
+return (
+      <div className="fish-container">
+        <div className="fish">
+        
+        </div>
+        <div className="fish">
+        
+        </div>
+      </div>
+    );
+  };
+  
